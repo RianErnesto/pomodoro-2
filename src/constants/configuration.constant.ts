217109ca4e4f18ff93b633f2f4889b2644bfb8a9
RianErnesto <rianernesto9@gmail.com>
2024-11-27 18:18:05 -0300

    :recycle: refactor: Type PomodoroConfiguration changed

--- src/constants/configuration.constant.ts
+++ src/constants/configuration.constant.ts
@@ -1,10 +1,10 @@
-import { PomodoroConfigurations } from "@/types/Configuration.type";
+import { ScreenSizesType } from "@/types/Configuration.type";
 
-export const DEFAULT_CONFIGURATION: PomodoroConfigurations = {
+export const DEFAULT_CONFIGURATION = {
   hideTimeOnTitle: false,
   initialRepeats: 1,
   alwaysStartWithInitialRepeats: true,
-  screenSize: "md",
+  screenSize: "md" as ScreenSizesType,
   displayVideo: true,
   muteVideo: false,
 };
--- src/types/Configuration.type.ts
+++ src/types/Configuration.type.ts
@@ -1,34 +1,4 @@
 export type ScreenSizesType = "sm" | "md" | "lg";
+import { DEFAULT_CONFIGURATION } from "@/constants/configuration.constant";
 
-export interface PomodoroConfigurations {
-  /**
-   * Hide time on title
-   * @default false
-   */
-  hideTimeOnTitle: boolean;
-  /**
-   * Display video on resting moment
-   * @default true
-   */
-  displayVideo: boolean;
-  /**
-   * Mute video on resting moment
-   * @default false
-   */
-  muteVideo: boolean;
-  /**
-   * Initial repeats
-   * @default 1
-   */
-  initialRepeats: number;
-  /**
-   * Always start with initial repeats
-   * @default true
-   */
-  alwaysStartWithInitialRepeats: boolean;
-  /**
-   * Screen size
-   * @default "md"
-   */
-  screenSize: ScreenSizesType;
-}
+export type PomodoroConfigurations = typeof DEFAULT_CONFIGURATION;
